c8e5a2061c1361cf099245f53c6b480abb2eba59
zamtovisk <91424361+zamtovisk@users.noreply.github.com>
2022-09-17 18:49:33 +0300

    Update primeN.py

--- primeN.py
+++ primeN.py
@@ -1,1 +1,11 @@
-
+import math
+n = 4
+#index is the count of dividers the number's got
+index = 0
+for counter in range(1, n+1):
+    if n/counter == math.trunc(n/counter):
+        index = index + 1
+if index > 2 or n == 1:
+    print('not a prime number')
+else:
+    print('prime numebr')
