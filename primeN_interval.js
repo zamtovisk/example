e5d08b964eebff401a87d7e8fa9d97ded16001fb
zamtovisk <91424361+zamtovisk@users.noreply.github.com>
2022-09-17 18:47:27 +0300

    Update primeN_interval.js

--- primeN_interval.js
+++ primeN_interval.js
@@ -1,1 +1,19 @@
-
+let start_number = 2;
+let finish_number = 20;
+let counter = 0;
+for(start_number; strat_number < finish_number; start_number++){
+  //i is divider for every number. It's there to help the counter
+  for(i = 1; i<= start_number; i++){
+    if(start_number/i == Math.trunc(start_number/i)){
+      counter++;
+    }
+  }
+if(counter > 2 || start_number == 1){
+    //print nothing and reset counter
+    counter = 0;
+}
+else{
+    console.log(start_number);
+    counter = 0;
+  }
+}
